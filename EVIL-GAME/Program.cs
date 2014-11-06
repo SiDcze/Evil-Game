b56f35946b2f07c542ec4d2eb80e18e5fdbcd4cb
DolanCZ <dolanovsky@gmail.com>
2014-11-06 17:16:34 +0100

    ADDED OTHER FILES

---
+++ EVIL-GAME/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVIL_GAME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int origWidth;
+            int origHeight; 
+            
+            origWidth = Console.WindowWidth;
+            origHeight = Console.WindowHeight;
+
+            Console.WriteLine("{origWidth / 2}", "Prostředek?");
+
+            Console.ReadKey();
+
+            
+   
+        }
+    }
+}
